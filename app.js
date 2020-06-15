e6020704ea6d16d0e25b3a0e24751ca7a018a4fa
wildfly8 <oubleknot14@aol.com>
2020-06-15 14:05:29 -0700

    re-structure for Heroku deployment

--- app.js
+++ app.js
@@ -15,19 +15,20 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('client/build'));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter); 
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+// If no API routes are hit, send the React app
+apiRouter.use(function(req, res) {
+	res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,11 +43,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 //setup mongoDB datasource
-mongoose.connect(process.env.MONGODB_URL || configDB.url, {
+mongoose.connect(process.env.MONGODB_URI || configDB.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
