a8df5755f1e126e9891d13bf4d67036ba178dd4e
wildfly8 <oubleknot14@aol.com>
2020-06-15 14:22:36 -0700

    heroku ready

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 const dbuser = 'heroku_7npd58d5';
-const dbpassword = '12345678Aa';
+const dbpassword = '1d5lrqnr2meghad3a2bp3ap02';
 
 module.exports = {
     'MONGODB_URI' : `mongodb://${dbuser}:${dbpassword}@ds223161.mlab.com:23161/heroku_7npd58d5`
