de159abae3642611cd12bf8fa4cb408ae4eaae45
wildfly8 <oubleknot14@aol.com>
2020-06-16 23:23:02 -0700

    index router fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
