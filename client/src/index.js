5a91aa5c144371500117455a6433a8183743c60d
wildfly8 <oubleknot14@aol.com>
2020-07-30 14:18:49 -0700

    clean unused files

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './polyfills';
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { MyContextProvider } from './MyContext';
 
